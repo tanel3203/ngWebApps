370cb8c70d00671224e9fb5c4165d2c573be6c85
tanel3203 <tanel3203@gmail.com>
2016-11-30 11:50:42 +0200

    w3

---
+++ W3_solution/app.js
@@ -0,0 +1,84 @@
+(function() {
+'use strict';
+
+angular.module('narrowitdownapp', [])
+  .controller('NarrowItDownController', NarrowItDownController)
+  .service('MenuSearchService', MenuSearchService)
+  .directive('foundItems', FoundItemsDirective);
+
+
+
+  function FoundItemsDirective () {
+  	var ddo = {
+  		templateUrl: 'narrowed.html',
+  		scope: {
+  			found: '<',
+  			onRemove: "&"
+  		},
+  		controller: NarrowItDownController,
+  		controllerAs: 'list',
+  		bindToController: true
+  	};
+
+  	return ddo;
+
+  }
+
+  NarrowItDownController.$inject = ['MenuSearchService'];
+
+  function NarrowItDownController (MenuSearchService) {
+  	var narrowItDown = this;
+
+  	narrowItDown.dataInput;
+
+  	narrowItDown.findData = function () {
+  	  console.log("Clicked");
+  	  var promise = MenuSearchService.getMatchedMenuItems(narrowItDown.dataInput);
+
+  	  promise.then(function (response) {
+  	  	narrowItDown.found = response;
+
+  	  	console.log("Vastus: ", response);
+  	  })
+  	  .catch(function (error) {
+  	  	console.log("Something went terribly wrong");
+  	  })
+  	};
+
+  	narrowItDown.remove = function (itemIndex) {
+  		console.log("Remove this one: ", itemIndex);
+  		narrowItDown.found.splice(itemIndex,1);
+  	};
+
+  };
+
+
+  MenuSearchService.$inject = ['$http'];
+
+  function MenuSearchService ($http) {
+  	var service = this;
+
+  	service.getMatchedMenuItems = function (searchTerm) {
+        return $http({
+        	method: "GET",
+        	url: ("http://davids-restaurant.herokuapp.com/menu_items.json")
+        }).then(function (result) {
+        // process result and only keep items that match
+        var foundItems = [];
+        var resultItems = result.data.menu_items;
+        for (var i = 0; i < resultItems.length; i++) {
+        	if (resultItems[i].name.indexOf(searchTerm) !== -1) {
+        		foundItems.push(resultItems[i]);
+        	}
+        }
+        console.log(foundItems);
+        // return processed items
+        return foundItems;
+        });
+  	}
+
+  };
+
+
+
+})();
